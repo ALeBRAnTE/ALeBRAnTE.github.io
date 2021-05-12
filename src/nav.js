88459a1b3c6e806087978ad1cfdeb21a0fbd0886
Lucas Alebrante <60799886+ALeBRAnTE@users.noreply.github.com>
2021-05-12 15:24:35 -0300

    up

--- src/nav.js
+++ src/nav.js
@@ -1,10 +1,10 @@
 function menu(){
-var CheckboxMenu = document.querySelector('#cb-menu');
-const menu = document.querySelector('.ul-menu');
+    var CheckboxMenu = document.querySelector('#cb-menu');
+    const menu = document.querySelector('.ul-menu');
 
-if (CheckboxMenu.checked == true){
-    menu.style.display = 'flex';
-} else {
-    menu.style.display = 'none';
-}
+    if (CheckboxMenu.checked == true){
+        menu.style.display = 'flex';
+    } else {
+        menu.style.display = 'none';
+    }
 }
